30abb9c5ead500a1cb4869a1d547e75dfb391c72
Duc Cuong <duccuong5199@gmail.com>
2014-02-01 23:01:23 +0700

    Initial commit

---
+++ src/main/DemoApplication.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Dai Hai
+ */
+public class DemoApplication {
+    
+    public static final int MY_CONSTANT = 10000;
+    
+    public static void main(String[] args) {
+        
+        System.out.println(MY_CONSTANT);
+    }
+}
